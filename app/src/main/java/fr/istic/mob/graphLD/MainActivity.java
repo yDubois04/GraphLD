655812b28aed9074196eee095390a9729be54e2d
Bolbi <bolbi.ket@gmail.com>
2019-10-17 18:02:14 +0200

    Merged my project

--- app/src/main/java/fr/istic/mob/graphLD/MainActivity.java
+++ app/src/main/java/fr/istic/mob/graphLD/MainActivity.java
@@ -3,8 +3,9 @@ package fr.istic.mob.graphLD;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.graphics.Canvas;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,9 +13,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawableGraph drawableGraph;
-    private Graph graph;
-    ImageView imageView;
+    private DrawableGraph graphView;
+    private Graph graphe;
+    ImageView imageGraph;
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -23,11 +24,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.initialiserGraph();
-        drawableGraph = new DrawableGraph(graph);
-        imageView = findViewById(R.id.imageView);
-        imageView.setImageDrawable(drawableGraph);
+        graphView = new DrawableGraph(graphe);
+        imageGraph = findViewById(R.id.imageView);
+        imageGraph.setImageDrawable(graphView);
 
-        imageView.setOnTouchListener(new View.OnTouchListener() {
+        imageGraph.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 final int action = motionEvent.getAction();
@@ -39,22 +40,22 @@ public class MainActivity extends AppCompatActivity {
                 if (action == MotionEvent.ACTION_DOWN && touchNode != null) {
                     touchNode.setCoordX((int)x);
                     touchNode.setCoordY((int)y);
-                    drawableGraph = new DrawableGraph(graph);
-                    imageView.setImageDrawable(drawableGraph);
+                    graphView = new DrawableGraph(graphe);
+                    imageGraph.setImageDrawable(graphView);
                 }
 
                 if ( action == MotionEvent.ACTION_MOVE && touchNode != null) {
                     touchNode.setCoordX((int)x);
                     touchNode.setCoordY((int)y);
-                    drawableGraph = new DrawableGraph(graph);
-                    imageView.setImageDrawable(drawableGraph);
+                    graphView = new DrawableGraph(graphe);
+                    imageGraph.setImageDrawable(graphView);
                 }
 
                 if ( action == MotionEvent.ACTION_UP && touchNode != null) {
                     touchNode.setCoordX((int)x);
                     touchNode.setCoordY((int)y);
-                    drawableGraph = new DrawableGraph(graph);
-                    imageView.setImageDrawable(drawableGraph);
+                    graphView = new DrawableGraph(graphe);
+                    imageGraph.setImageDrawable(graphView);
                 }
 
                 return true;
@@ -62,10 +63,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void initialGraph(){
+        graphe = new Graph();
+        for(int i = 0; i < 9; i++){
+            graphe.addNode(new Node(i * 120, i * 120, "blue", ""));
+        }
+    }
+
     public Node findTouchNode (float coordX, float coordY) {
         Node n = null;
 
-        for (Node node : graph.getNodes()) {
+        for (Node node : graphe.getNodes()) {
             if (node.contains(coordX,coordY)) {
                 return node;
             }
@@ -74,9 +82,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initialiserGraph () {
-        graph = new Graph();
+        graphe = new Graph();
         for (int i = 0; i < 9; i++) {
-            graph.getNodes().add(new Node(120f*i, 0, "noir","noeud"+i));
+            graphe.getNodes().add(new Node(120f*i, 0, "noir","noeud"+i));
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemID = item.getItemId();
+        if(itemID == R.id.resetButton){
+            Toast.makeText(getApplicationContext(),"RESET", Toast.LENGTH_SHORT).show();
+            initialGraph();
+            update();
+        }
+        if(itemID == R.id.delButton){
+            Toast.makeText(getApplicationContext(),"DELETE", Toast.LENGTH_SHORT).show();
+            graphe = new Graph();
+            update();
+        }
+        else if(itemID == R.id.addNodeButton){
+            Toast.makeText(getApplicationContext(),"NOEUD", Toast.LENGTH_SHORT).show();
+            graphe.addNode(new Node(300, 300, "blue", ""));
+            update();
+        }
+        else if(itemID == R.id.addArcButton){
+            Toast.makeText(getApplicationContext(),"ARC", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void update(){
+        graphView = new DrawableGraph(graphe);
+        imageGraph.setImageDrawable(graphView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
